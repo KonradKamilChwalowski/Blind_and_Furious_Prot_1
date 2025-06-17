20089892990dea59f75f0715497e5b5906e40d29
KonradKamilChwalowski <ace55@wp.pl>
2025-06-17 16:49:58 +0200

    Add late options for driving command

--- scripts/car.gd
+++ scripts/car.gd
@@ -2,12 +2,33 @@ extends Node2D
 
 @export var speed: float = 100.0  # prędkość w pikselach na sekundę
 
+var waiting_time: float
+var rotation_degree: float = deg_to_rad(0)
+
 func _process(delta: float) -> void:
 	# Ruch do przodu w kierunku, w którym obrócony jest pojazd (lokalna oś Y)
 	position -= transform.y.normalized() * speed * delta
 
+
+
 func _on_button_left_pressed() -> void:
-	rotation += deg_to_rad(-90)
+	rotation_degree = deg_to_rad(-90)
 	
 func _on_button_right_pressed() -> void:
-	rotation += deg_to_rad(90)
+	rotation_degree = deg_to_rad(90)
+
+func _on_button_000_pressed() -> void:
+	waiting_time = 0.0
+	
+func _on_button_100_pressed() -> void:
+	waiting_time = 1.0
+
+func _on_button_200_pressed() -> void:
+	waiting_time = 2.0
+
+func _on_button_300_pressed() -> void:
+	waiting_time = 3.0
+
+func _on_add_driving_command_pressed() -> void:
+	await get_tree().create_timer(waiting_time).timeout  # czeka 2 sekundy
+	rotation += rotation_degree
