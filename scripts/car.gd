89e674ce4c5e96fd74cdf4470cb9bc609337ce84
KonradKamilChwalowski <ace55@wp.pl>
2025-06-17 17:28:44 +0200

    Add Command_List and Label for it

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,9 +1,15 @@
-extends Node2D
+class_name Car extends Node2D
 
 @export var speed: float = 100.0  # prędkość w pikselach na sekundę
 
 var waiting_time: float
-var rotation_degree: float = deg_to_rad(0)
+var rotation_degree: float = 0.0
+var command_queue: Array = []
+var is_executing: bool = false
+var command_list_container: VBoxContainer
+
+func init(cls: VBoxContainer) -> void:
+	command_list_container = cls
 
 func _process(delta: float) -> void:
 	# Ruch do przodu w kierunku, w którym obrócony jest pojazd (lokalna oś Y)
@@ -11,11 +17,12 @@ func _process(delta: float) -> void:
 
 
 
+#BUTTONS
 func _on_button_left_pressed() -> void:
-	rotation_degree = deg_to_rad(-90)
+	rotation_degree = -90
 	
 func _on_button_right_pressed() -> void:
-	rotation_degree = deg_to_rad(90)
+	rotation_degree = 90
 
 func _on_button_000_pressed() -> void:
 	waiting_time = 0.0
@@ -30,5 +37,33 @@ func _on_button_300_pressed() -> void:
 	waiting_time = 3.0
 
 func _on_add_driving_command_pressed() -> void:
-	await get_tree().create_timer(waiting_time).timeout  # czeka 2 sekundy
-	rotation += rotation_degree
+	# ADD new type var - "command", wchich is 2D (delay, rotation) type - to the array
+	var command = {
+		"delay": waiting_time,
+		"rotation": rotation_degree}
+	command_queue.append(command)
+	
+	# Add command to the Label
+	var label := Label.new()
+	label.text = "Za %.1fs: skręć o %d°" % [waiting_time, rotation_degree]
+	command_list_container.add_child(label)
+	
+	#if there was no command before, execute immediatly
+	if not is_executing:
+		execute_next_command()
+
+func execute_next_command() -> void:
+	# If empty, than there is nothing to do
+	if command_queue.is_empty():
+		is_executing = false
+		return
+	# Not empty
+	is_executing = true
+	var cmd = command_queue.pop_front()
+	if command_list_container.get_child_count() > 0:
+		command_list_container.get_child(0).queue_free()
+		
+	await get_tree().create_timer(cmd.delay).timeout
+	rotation += deg_to_rad(cmd.rotation)
+	
+	execute_next_command()
--- scripts/race_manager.gd
+++ scripts/race_manager.gd
@@ -1,13 +1,13 @@
 class_name RaceManager extends Node2D
 
 @onready var track_manager: TrackManager = $TrackManager
+@onready var command_list_container: VBoxContainer = $"UI/CommandList"
+@onready var car: Car = $Car
 
 var starting_position = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	track_manager.init(starting_position)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	car.init(command_list_container)
+	
